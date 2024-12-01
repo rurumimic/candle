4d2550703ef468323bc7fa1e106d66b4a4057491
rurumimic <10915235+rurumimic@users.noreply.github.com>
2024-12-01 22:26:15 +0900

    to Linear Layer

--- src/mnist/src/main.rs
+++ src/mnist/src/main.rs
@@ -1,23 +1,43 @@
 use candle_core::{Device, Result, Tensor};
 
+struct Linear {
+    weight: Tensor,
+    bias: Tensor,
+}
+
+impl Linear {
+    fn forward(&self, x: &Tensor) -> Result<Tensor> {
+        let x = x.matmul(&self.weight)?;
+        x.broadcast_add(&self.bias)
+    }
+}
+
 struct Model {
-    first: Tensor,
-    second: Tensor,
+    first: Linear,
+    second: Linear,
 }
 
 impl Model {
     fn forward(&self, image: &Tensor) -> Result<Tensor> {
-        let x = image.matmul(&self.first)?;
+        let x = self.first.forward(image)?;
         let x = x.relu()?;
-        x.matmul(&self.second)
+        self.second.forward(&x)
     }
 }
 
 fn main() -> Result<()> {
-    let device = Device::new_cuda(0)?;
+    // let device = Device::Cpu;
+    // let device = Device::new_cuda(0)?;
+    let device = Device::cuda_if_available(0)?;
 
-    let first = Tensor::randn(0f32, 1.0, (784, 100), &device)?;
-    let second = Tensor::randn(0f32, 1.0, (100, 10), &device)?;
+    let weight = Tensor::randn(0f32, 1.0, (784, 100), &device)?;
+    let bias = Tensor::randn(0f32, 1.0, (100,), &device)?;
+    let first = Linear { weight, bias };
+
+    let weight = Tensor::randn(0f32, 1.0, (100, 10), &device)?;
+    let bias = Tensor::randn(0f32, 1.0, (10,), &device)?;
+    let second = Linear { weight, bias };
+
     let model = Model { first, second };
 
     let dummy_image = Tensor::randn(0f32, 1.0, (1, 784), &device)?;
