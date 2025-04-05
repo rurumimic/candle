4348c99941e71a50b6ce5e47eb4a12d8be1b235a
rurumimic <10915235+rurumimic@users.noreply.github.com>
2025-04-05 12:57:11 +0900

    roberta embedding

--- src/candle-roberta/src/main.rs
+++ src/candle-roberta/src/main.rs
@@ -1,9 +1,15 @@
-use anyhow::{Error, Result};
-use candle_core::{DType, Device};
+use anyhow::Result;
+use candle_core::{DType, Device, Tensor};
 use candle_nn::VarBuilder;
 use candle_roberta::models::roberta::{RobertaConfig, RobertaModel};
-use hf_hub::{Cache, Repo};
+use hf_hub::Repo;
+use tokenizers::tokenizer::Encoding;
 use tokenizers::Tokenizer;
+
+fn round_to_decimal_places(n: f32, places: u32) -> f32 {
+    let multiplier: f32 = 10f32.powi(places as i32);
+    (n * multiplier).round() / multiplier
+}
 
 fn main() -> Result<()> {
     let device = Device::Cpu;
@@ -14,16 +20,63 @@ fn main() -> Result<()> {
     let config_filename = std::fs::read_to_string(config_filename)?;
     let config: RobertaConfig = serde_json::from_str(&config_filename)?;
 
-    let tokenizer_filename =
-        "models/cardiffnlp/twitter-roberta-base-sentiment-latest/tokenizer.json";
-    let tokenizer = Tokenizer::from_file(tokenizer_filename).map_err(anyhow::Error::msg)?;
+    //let tokenizer_filename = "models/cardiffnlp/twitter-roberta-base-sentiment-latest/tokenizer.json";
+    //let tokenizer = Tokenizer::from_file(tokenizer_filename).map_err(anyhow::Error::msg)?;
 
+    //let weights_filename =
+    //    "models/cardiffnlp/twitter-roberta-base-sentiment-latest/model.safetensors";
+    //let weights =
+    //    unsafe { VarBuilder::from_mmaped_safetensors(&[weights_filename], DType::F32, &device)? };
+    //
     let weights_filename =
-        "models/cardiffnlp/twitter-roberta-base-sentiment-latest/model.safetensors";
+        "models/cardiffnlp/twitter-roberta-base-sentiment-latest/pytorch_model.bin";
     let weights =
-        unsafe { VarBuilder::from_mmaped_safetensors(&[weights_filename], DType::F32, &device)? };
+        VarBuilder::from_pth(&weights_filename, DType::F32, &device)?.set_prefix("roberta");
 
     let model = RobertaModel::load(weights, &config)?;
+
+    //let input = "I love programming in Rust!";
+    //let encoding = tokenizer.encode(input, false).map_err(anyhow::Error::msg)?;
+    //println!("Encoding: {:?}", encoding);
+
+    //let input_ids = encoding.get_ids();
+    let input_ids = &[[0u32, 31414, 232, 328, 740, 1140, 12695, 69, 46078, 1588, 2]];
+    println!("Input IDs: {:?}", input_ids);
+
+    let input_ids = Tensor::new(input_ids, &device)?;
+    let token_ids = input_ids.zeros_like()?;
+
+    let output = model.forward(
+        &input_ids,
+        Some(&token_ids),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        false,
+        false,
+        false,
+    )?;
+
+    println!("Output: {:?}", output);
+
+    let output = output.squeeze(0)?;
+    let output = output.to_vec2::<f32>()?;
+    let output: Vec<Vec<f32>> = output
+        .iter()
+        .take(12)
+        .map(|nested_vec| {
+            nested_vec
+                .iter()
+                .take(4)
+                .map(|&x| round_to_decimal_places(x, 4))
+                .collect()
+        })
+        .collect();
+    println!("Output: {:?}", output);
 
     Ok(())
 }
--- src/candle-roberta/src/models/roberta.rs
+++ src/candle-roberta/src/models/roberta.rs
@@ -2,10 +2,11 @@ use std::collections::HashMap;
 
 use anyhow::{Error, Result};
 use candle_core::scalar::{TensorOrScalar, TensorScalar};
-use candle_core::{DType, Tensor, D};
+use candle_core::{DType, Device, Tensor, D};
 use candle_nn::ops::softmax;
 use candle_nn::{
-    embedding, layer_norm, linear, Dropout, Embedding, LayerNorm, Linear, Module, VarBuilder,
+    embedding, layer_norm, linear, Activation, Dropout, Embedding, LayerNorm, Linear, Module,
+    VarBuilder,
 };
 use serde::Deserialize;
 
@@ -27,13 +28,13 @@ pub struct RobertaConfig {
     bos_token_id: usize,
     eos_token_id: usize,
     position_embedding_type: String,
-    use_cache: bool,
+    use_cache: Option<bool>,
     classifier_dropout: Option<f64>,
     model_type: Option<String>,
     _num_labels: Option<usize>,
     id2label: Option<HashMap<String, String>>,
     label2id: Option<HashMap<String, usize>>,
-    is_decoder: bool,
+    is_decoder: Option<bool>,
 }
 
 impl Default for RobertaConfig {
@@ -55,27 +56,104 @@ impl Default for RobertaConfig {
             bos_token_id: 0,
             eos_token_id: 2,
             position_embedding_type: "absolute".to_string(),
-            use_cache: true,
+            use_cache: Some(true),
             classifier_dropout: None,
             model_type: Some("roberta".to_string()),
             _num_labels: Some(3),
             id2label: None,
             label2id: None,
-            is_decoder: false,
+            is_decoder: Some(false),
         }
     }
 }
 
 pub struct RobertaModel {
-    pub padding_idx: u32,
+    embeddings: RobertaEmbeddings,
+    encoder: RobertaEncoder,
+    pub device: Device,
 }
 
 impl RobertaModel {
     pub fn load(vb: VarBuilder, config: &RobertaConfig) -> Result<Self> {
+        let embeddings = RobertaEmbeddings::load(vb.pp("embeddings"), config)?;
+        let encoder = RobertaEncoder::load(vb.pp("encoder"), config)?;
+        let device = vb.device().clone();
+
         Ok(Self {
-            padding_idx: config.pad_token_id as u32,
+            embeddings,
+            encoder,
+            device,
         })
     }
+
+    pub fn forward(
+        &self,
+        input_ids: &Tensor,
+        attention_mask: Option<&Tensor>,
+        token_type_ids: Option<&Tensor>,
+        position_ids: Option<&Tensor>,
+        head_mask: Option<&Tensor>,
+        inputs_embeds: Option<&Tensor>,
+        encoder_hidden_states: Option<&Tensor>,
+        encoder_attention_mask: Option<&Tensor>,
+        past_key_values: Option<(&Tensor, &Tensor)>,
+        output_attentions: bool,
+        output_hidden_states: bool,
+        return_dict: bool,
+    ) -> Result<Tensor> {
+        let embedding_output = self.embeddings.forward(
+            input_ids,
+            token_type_ids.unwrap_or(&Tensor::zeros_like(input_ids)?),
+            None,
+            None,
+            0u8,
+        )?;
+
+        let sequence_output = self.encoder.forward(
+            &embedding_output,
+            attention_mask,
+            head_mask,
+            encoder_hidden_states,
+            encoder_attention_mask,
+            past_key_values,
+            output_attentions,
+        )?;
+
+        Ok(sequence_output)
+    }
+}
+
+fn cumsum_2d(mask: &Tensor, dim: u8, device: &Device) -> Result<Tensor> {
+    let mask = mask.to_vec2::<u8>()?;
+
+    let rows = mask.len();
+    let cols = mask[0].len();
+
+    let mut result = mask.clone();
+
+    match dim {
+        0 => {
+            // Cumulative sum along rows
+            for i in 0..rows {
+                for j in 1..cols {
+                    result[i][j] += result[i][j - 1];
+                }
+            }
+        }
+        1 => {
+            // Cumulative sum along columns
+            for j in 0..cols {
+                for i in 1..rows {
+                    result[i][j] += result[i - 1][j];
+                }
+            }
+        }
+        _ => panic!("Dimension not supported"),
+    }
+
+    let result = Tensor::new(result, &device)?;
+
+    Ok(result)
 }
 
 fn create_position_ids_from_input_ids(
@@ -84,11 +162,12 @@ fn create_position_ids_from_input_ids(
     past_key_values_length: u8,
 ) -> Result<Tensor> {
     let mask = input_ids.ne(padding_idx)?;
-    let incremental_indices = mask
-        .cumsum(1)?
-        .broadcast_add(&Tensor::new(&[past_key_values_length], input_ids.device())?)?
-        .mul(&mask)?
-        .broadcast_add(&Tensor::new(&[padding_idx], input_ids.device())?)?;
+    let incremental_indices = cumsum_2d(&mask, 0, input_ids.device())?;
+    let incremental_indices = incremental_indices
+        .broadcast_add(&Tensor::new(&[past_key_values_length], input_ids.device())?)?;
+    let incremental_indices = incremental_indices.mul(&mask)?.to_dtype(DType::U32)?;
+    let incremental_indices =
+        incremental_indices.broadcast_add(&Tensor::new(&[padding_idx], input_ids.device())?)?;
 
     Ok(incremental_indices)
 }
@@ -150,15 +229,39 @@ impl RobertaEmbeddings {
         &self,
         input_ids: &Tensor,
         token_type_ids: &Tensor,
-        position_ids: &Tensor,
-        inputs_embeds: &Tensor,
+        position_ids: Option<&Tensor>,
+        inputs_embeds: Option<&Tensor>,
         past_key_values_length: u8,
     ) -> Result<Tensor> {
-        //let position_ids = self.create_position_ids_from_inputs_embeds(inputs_embeds);
+        let position_ids = match position_ids {
+            Some(ids) => ids.to_owned(),
+            None => {
+                if Option::is_some(&inputs_embeds) {
+                    self.create_position_ids_from_inputs_embeds(inputs_embeds.unwrap())?
+                } else {
+                    create_position_ids_from_input_ids(
+                        input_ids,
+                        self.padding_idx,
+                        past_key_values_length,
+                    )?
+                }
+            }
+        };
+
+        let inputs_embeds: Tensor = match inputs_embeds {
+            Some(embeds) => embeds.to_owned(),
+            None => self.word_embeddings.forward(input_ids)?,
+        };
 
         let token_type_embeddings = self.token_type_embeddings.forward(&token_type_ids)?;
 
-        let mut embeddings = (inputs_embeds + token_type_embeddings)?;
+        let mut embeddings = inputs_embeds.add(&token_type_embeddings)?;
+
+        if let Some(position_embeddings) = &self.position_embeddings {
+            let position_embeddings = position_embeddings.forward(&position_ids)?;
+            embeddings = embeddings.broadcast_add(&position_embeddings)?;
+        }
+
         embeddings = self.layer_norm.forward(&embeddings)?;
         //embeddings = self.dropout.forward(&embeddings, false)?;
         Ok(embeddings)
@@ -190,8 +293,8 @@ struct RobertaSelfAttention {
     dropout: Dropout,
     position_embedding_type: Option<String>,
     max_position_embeddings: usize,
-    distance_embedding: Embedding,
-    is_decoder: bool,
+    //distance_embedding: Embedding,
+    is_decoder: Option<bool>,
 }
 
 impl RobertaSelfAttention {
@@ -205,11 +308,11 @@ impl RobertaSelfAttention {
         let dropout = Dropout::new(config.attention_probs_dropout_prob as f32);
         let position_embedding_type = config.position_embedding_type.clone();
         let max_position_embeddings = config.max_position_embeddings;
-        let distance_embedding = embedding(
-            2 * config.max_position_embeddings + 1,
-            attention_head_size,
-            vb.pp("distance_embedding"),
-        )?;
+        //let distance_embedding = embedding(
+        //    2 * config.max_position_embeddings + 1,
+        //    attention_head_size,
+        //    vb.pp("distance_embedding"),
+        //)?;
         let is_decoder = config.is_decoder;
 
         Ok(Self {
@@ -222,14 +325,15 @@ impl RobertaSelfAttention {
             dropout,
             position_embedding_type: Some(position_embedding_type),
             max_position_embeddings,
-            distance_embedding,
+            //distance_embedding,
             is_decoder,
         })
     }
 
     fn transpose_for_scores(&self, x: &Tensor) -> Result<Tensor> {
         let mut new_x_shape = x.dims().to_vec();
-        new_x_shape[1] = self.num_attention_heads;
+        new_x_shape.pop();
+        new_x_shape.push(self.num_attention_heads);
         new_x_shape.push(self.attention_head_size);
         let x = x.reshape(new_x_shape)?;
         let x = x.permute((0, 2, 1, 3))?;
@@ -291,10 +395,10 @@ impl RobertaSelfAttention {
         //let attention_scores = query_layer.matmul(&key_layer.t()?)?;
         let attention_scores = query_layer.matmul(&key_layer.transpose(2, 3)?)?;
         let attention_scores = (attention_scores / (self.attention_head_size as f64).sqrt())?;
-        let attention_scores = match attention_mask {
-            Some(mask) => attention_scores.add(&mask)?,
-            None => attention_scores,
-        };
+        //let attention_scores = match attention_mask {
+        //    Some(mask) => attention_scores.add(&mask)?,
+        //    None => attention_scores,
+        //};
 
         let attention_probs = softmax(&attention_scores, D::Minus1)?;
         let attention_probs = self.dropout.forward(&attention_probs, false)?;
@@ -314,5 +418,214 @@ impl RobertaSelfAttention {
         let context_layer = context_layer.reshape(new_context_layer_shape)?;
 
         Ok(context_layer)
+    }
+}
+
+struct RobertaSelfOutput {
+    dense: Linear,
+    layer_norm: LayerNorm,
+}
+
+impl RobertaSelfOutput {
+    fn load(vb: VarBuilder, config: &RobertaConfig) -> Result<Self> {
+        let dense = linear(config.hidden_size, config.hidden_size, vb.pp("dense"))?;
+        let layer_norm = layer_norm(
+            config.hidden_size,
+            config.layer_norm_eps,
+            vb.pp("LayerNorm"),
+        )?;
+
+        Ok(Self { dense, layer_norm })
+    }
+
+    fn forward(&self, hidden_states: &Tensor, input_tensor: &Tensor) -> Result<Tensor> {
+        let hidden_states = self.dense.forward(hidden_states)?;
+        let hidden_states = hidden_states.add(input_tensor)?;
+        let hidden_states = self.layer_norm.forward(&hidden_states)?;
+        Ok(hidden_states)
+    }
+}
+
+struct RobertaAttention {
+    self_attention: RobertaSelfAttention,
+    output: RobertaSelfOutput,
+}
+
+impl RobertaAttention {
+    fn load(vb: VarBuilder, config: &RobertaConfig) -> Result<Self> {
+        let self_attention = RobertaSelfAttention::load(vb.pp("self"), config)?;
+        let output = RobertaSelfOutput::load(vb.pp("output"), config)?;
+
+        Ok(Self {
+            self_attention,
+            output,
+        })
+    }
+
+    fn forward(
+        &self,
+        hidden_states: &Tensor,
+        attention_mask: Option<&Tensor>,
+        head_mask: Option<&Tensor>,
+        encoder_hidden_states: Option<&Tensor>,
+        encoder_attention_mask: Option<&Tensor>,
+        past_key_value: Option<(&Tensor, &Tensor)>,
+        output_attentions: bool,
+    ) -> Result<Tensor> {
+        let self_outputs = self.self_attention.forward(
+            hidden_states,
+            attention_mask,
+            head_mask,
+            encoder_hidden_states,
+            encoder_attention_mask,
+            past_key_value,
+            output_attentions,
+        )?;
+
+        let attention_output = self.output.forward(&self_outputs, hidden_states)?;
+        Ok(attention_output)
+    }
+}
+
+struct RobertaIntermediate {
+    dense: Linear,
+    intermediate_act_fn: Activation,
+}
+
+impl RobertaIntermediate {
+    fn load(vb: VarBuilder, config: &RobertaConfig) -> Result<Self> {
+        let dense = linear(config.hidden_size, config.intermediate_size, vb.pp("dense"))?;
+        //let intermediate_act_fn = serde_json::from_str(&config.hidden_act)?;
+        let intermediate_act_fn = match config.hidden_act.as_str() {
+            "gelu" => Activation::Gelu,
+            //"relu" => Activation::Relu,
+            //"swish" => Activation::Swish,
+            _ => return Err(Error::msg("Unsupported activation function")),
+        };
+
+        Ok(Self {
+            dense,
+            intermediate_act_fn,
+        })
+    }
+
+    fn forward(&self, hidden_states: &Tensor) -> Result<Tensor> {
+        let hidden_states = self.dense.forward(hidden_states)?;
+        let hidden_states = self.intermediate_act_fn.forward(&hidden_states)?;
+        Ok(hidden_states)
+    }
+}
+
+struct RobertaOutput {
+    dense: Linear,
+    layer_norm: LayerNorm,
+}
+
+impl RobertaOutput {
+    fn load(vb: VarBuilder, config: &RobertaConfig) -> Result<Self> {
+        let dense = linear(config.intermediate_size, config.hidden_size, vb.pp("dense"))?;
+        let layer_norm = layer_norm(
+            config.hidden_size,
+            config.layer_norm_eps,
+            vb.pp("LayerNorm"),
+        )?;
+
+        Ok(Self { dense, layer_norm })
+    }
+
+    fn forward(&self, hidden_states: &Tensor, input_tensor: &Tensor) -> Result<Tensor> {
+        let hidden_states = self.dense.forward(hidden_states)?;
+        let hidden_states = hidden_states.add(input_tensor)?;
+        let hidden_states = self.layer_norm.forward(&hidden_states)?;
+        Ok(hidden_states)
+    }
+}
+
+struct RobertaLayer {
+    attention: RobertaAttention,
+    intermediate: RobertaIntermediate,
+    output: RobertaOutput,
+}
+
+impl RobertaLayer {
+    fn load(vb: VarBuilder, config: &RobertaConfig) -> Result<Self> {
+        let attention = RobertaAttention::load(vb.pp("attention"), config)?;
+        let intermediate = RobertaIntermediate::load(vb.pp("intermediate"), config)?;
+        let output = RobertaOutput::load(vb.pp("output"), config)?;
+
+        Ok(Self {
+            attention,
+            intermediate,
+            output,
+        })
+    }
+
+    fn forward(
+        &self,
+        hidden_states: &Tensor,
+        attention_mask: Option<&Tensor>,
+        head_mask: Option<&Tensor>,
+        encoder_hidden_states: Option<&Tensor>,
+        encoder_attention_mask: Option<&Tensor>,
+        past_key_value: Option<(&Tensor, &Tensor)>,
+        output_attentions: bool,
+    ) -> Result<Tensor> {
+        let attention_output = self.attention.forward(
+            hidden_states,
+            attention_mask,
+            head_mask,
+            encoder_hidden_states,
+            encoder_attention_mask,
+            past_key_value,
+            output_attentions,
+        )?;
+
+        let intermediate_output = self.intermediate.forward(&attention_output)?;
+        let layer_output = self
+            .output
+            .forward(&intermediate_output, &attention_output)?;
+
+        Ok(layer_output)
+    }
+}
+
+struct RobertaEncoder {
+    layers: Vec<RobertaLayer>,
+}
+
+impl RobertaEncoder {
+    fn load(vb: VarBuilder, config: &RobertaConfig) -> Result<Self> {
+        let layers = (0..config.num_hidden_layers)
+            .map(|i| RobertaLayer::load(vb.pp(format!("layer.{i}")), config))
+            .collect::<Result<Vec<_>>>()?;
+
+        Ok(Self { layers })
+    }
+
+    fn forward(
+        &self,
+        hidden_states: &Tensor,
+        attention_mask: Option<&Tensor>,
+        head_mask: Option<&Tensor>,
+        encoder_hidden_states: Option<&Tensor>,
+        encoder_attention_mask: Option<&Tensor>,
+        past_key_value: Option<(&Tensor, &Tensor)>,
+        output_attentions: bool,
+    ) -> Result<Tensor> {
+        let mut hidden_states = hidden_states.clone();
+
+        for layer in self.layers.iter() {
+            hidden_states = layer.forward(
+                &hidden_states,
+                attention_mask,
+                head_mask,
+                encoder_hidden_states,
+                encoder_attention_mask,
+                past_key_value,
+                output_attentions,
+            )?;
+        }
+
+        Ok(hidden_states)
     }
 }
